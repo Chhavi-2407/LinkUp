275e7b6b2ad554c59ac5d1ebbf8761189c06a501
Chhavi <shridharchhavi24@gmail.com>
2026-03-29 04:20:17 +0530

    signup page added

--- js/auth.js
+++ js/auth.js
@@ -3,13 +3,42 @@ if(loginBtn){
     loginBtn.addEventListener("click",()=>{
         const email = document.getElementById("loginEmail").value;
         const password = document.getElementById("loginPassword").value;
+
+        if (!email || !password){
+            alert("Please fill all fields");
+            return;
+        }
         const user = JSON.parse(localStorage.getItem("user"));
-        // “authentication is handled using localStorage for demo purposes, since backend is not implemented.”
+        // authentication is handled using localStorage for demo purposes, since backend is not implemented.
         if (user && email === user.email && password === user.password){
+            localStorage.setItem("currentUser", user.name);
+            alert("Login successful!!")
             window.location.href = "dashboard.html";
         }
         else{
             alert("Invalid credentials!!");
         }
+    })
+}
+
+const signupBtn = document.getElementById("signupBtn");
+if (signupBtn){
+    signupBtn.addEventListener("click",()=>{
+        const name = document.getElementById("signupName").value;
+        const email = document.getElementById("signupEmail").value;
+        const password = document.getElementById("signupPassword").value;
+        // const user = {
+        //     name: name,
+        //     email: email,
+        //     password: password
+        // }
+        if(!name.trim() || !email.trim() || !password.trim()){
+            alert("Please fill all fields");
+            return;
+        }
+        const user = {name, email, password}
+        localStorage.setItem("user",JSON.stringify(user));
+        alert("Signup successful!!");
+        window.location.href = "login.html"
     })
 }
